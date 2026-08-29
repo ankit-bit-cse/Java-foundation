b33f7cd1f6cbce0729bc8dc208b0d068675e7179
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-08-29 22:35:38 +0530

    day1

--- Day-01-Java-Basics/HelloWorld.java
+++ Day-01-Java-Basics/HelloWorld.java
@@ -2,8 +2,16 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-       System.out.println("Java");
-System.out.println("is");
-System.out.println("awesome");
+     String name = "Ankit KUmar";
+     String college ="BIT";
+     int marks = 700;
+     boolean isPassed = true;
+
+
+     System.out.println("Name: "+name);
+     System.out.println("College: "+college);    
+     System.out.println("Marks: "+marks);
+     System.out.println("Is Passed: "+isPassed);
+
     }
 }
