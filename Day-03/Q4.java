ce52c53671b5d16ef12614e783edad64dbd7aa71
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-08-31 23:21:20 +0530

    for while loop

---
+++ Day-03/Q4.java
@@ -0,0 +1,16 @@
+class Q4{
+    public static void main(String[] args){
+        int i=1;
+        while(i<=100) {
+
+            if(i%2==0){
+                            System.out.println(i);
+
+
+            }
+            i++;
+
+        }
+
+    }
+}
