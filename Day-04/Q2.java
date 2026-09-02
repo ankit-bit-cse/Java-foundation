4fa86444dc9a7008e1b63c7077a69a0e7d871296
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-09-02 22:58:45 +0530

    array string

---
+++ Day-04/Q2.java
@@ -0,0 +1,9 @@
+class Q2 {
+    public static void main(String[] args) {
+        String name ="ANKIT";
+
+        for(int i=0;i< name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
+    }
+}
