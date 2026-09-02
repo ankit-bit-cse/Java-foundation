9df4939e3b35cb86499461b66a04823005fac0a4
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-09-02 23:07:31 +0530

    array sum

---
+++ Day-04/Q3.java
@@ -0,0 +1,11 @@
+class Q3 {
+    public static void main(String[] args) {
+        int[] marks={22,12,3,45};
+        int sum=0;
+
+        for(int i=0; i<marks.length; i++) {
+            sum= sum+marks[i];
+        }
+        System.out.println(sum);
+    }
+}
