5bd55b42c0113e39162029b1baf1a22e2e29c6a7
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-09-05 00:06:17 +0530

    2D Array

---
+++ Day-03/Q6.java
@@ -0,0 +1,24 @@
+class Q6 {
+    public static void main(String[] args) {
+        int[][] array={
+            {
+                10,20,30
+
+            },{
+                40,50,70
+            },{
+                78,45,33
+            }
+
+
+        };
+        for(int i=0;i<array.length;i++) {
+            for(int j=0; j<array[0].length;j++) {
+                System.out.println(array[1][2]);
+                return;
+            }
+
+        }
+
+    }
+}
