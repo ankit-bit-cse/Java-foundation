750858d9be1f63837f5ce33c3863aa2361d407b8
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-09-05 19:10:53 +0530

    print even number in array

---
+++ Day-04/Q6.java
@@ -0,0 +1,15 @@
+class Q6 {
+
+    public static void main(String[] args) {
+
+        int numbers[] = {10,20,30,45,65,40,50};
+
+        for(int i=0; i<numbers.length; i++) {
+
+           if (numbers[i] % 2 == 0)   {
+
+            System.out.println(numbers[i]);
+           }
+        }
+    }
+}
