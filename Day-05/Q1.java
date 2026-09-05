8e23258f2034c99e8249ff78872b9697da4e8f9e
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-09-05 23:28:37 +0530

    say hello

---
+++ Day-05/Q1.java
@@ -0,0 +1,16 @@
+class Q1 {
+
+    static void sayHello () { 
+
+        System.out.println("Hello");
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        sayHello() ;
+    }
+
+}
