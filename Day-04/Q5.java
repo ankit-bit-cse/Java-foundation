eeb1d41ef0013aaa3ea6cabefb506128e129c2d4
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-09-05 18:44:22 +0530

    element of array

---
+++ Day-04/Q5.java
@@ -0,0 +1,9 @@
+class Q5 {
+    public static void main(String[] args) {
+        int numbers[] = {10,20,30,40,50};
+
+        for(int i=0; i<numbers.length; i++) {
+            System.out.println("Index" + i + "=" + numbers[i]);
+        }
+    }
+}
