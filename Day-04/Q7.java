c24f47e16849317a01ecc291cb2c0c3096d6c5cf
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-09-05 19:21:18 +0530

    sum of array

---
+++ Day-04/Q7.java
@@ -0,0 +1,18 @@
+class Q7 {
+
+    public static void main(String[] args) {
+
+        int numbers[] = {10,20,30,45,65,40,50};
+
+        int sum = 0;
+
+        for(int i=0; i<numbers.length; i++) {
+
+            sum = sum + numbers[i];
+
+        }
+            System.out.println("sum =" +sum);
+           
+        }
+}
+    
