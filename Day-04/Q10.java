0181c7f46f0f70bae461638e7b25a2369fe43bf0
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-09-05 20:45:04 +0530

    String name

---
+++ Day-04/Q10.java
@@ -0,0 +1,12 @@
+class Q10 {
+
+    public static void main(String[] args) {
+
+        String name = "ANKIT";
+
+        for(int i=0; i<name.length(); i++) {
+
+            System.out.println(name.charAt(i));
+        }
+    }
+}
