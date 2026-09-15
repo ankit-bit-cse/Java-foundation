0646de896b29bd3a340bbded6e863c7075a9fe82
ankit-bit-cse <ankit.bit.cse@gmail.com>
2026-09-15 19:17:41 +0530

    Employee class with id

---
+++ classroom/Employee.java
@@ -0,0 +1,23 @@
+class Employee {
+    int id;
+    String name;
+    double salary;
+
+    Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void display() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee e = new Employee(101, "Rahul", 25000);
+
+        e.display();
+    }
+}
